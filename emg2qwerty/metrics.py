78a188f7ccff3e961e7d4c36b807bd11119e1659
Viswanath Sivakumar <viswanath@fb.com>
2021-08-16 18:44:57 -0700

    Initial commit

---
+++ emg2qwerty/metrics.py
@@ -0,0 +1,36 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import Levenshtein
+import torch
+from torchmetrics import Metric
+
+from emg2qwerty.data import LabelData
+
+
+class CharacterErrorRate(Metric):
+    """Character error rate (CER) metric by computing the Levenshtein
+    edit-distance between the predicted and target sequences.
+
+    As an instance of ``torchmetric.Metric``, synchronization across all GPUs
+    involved in a distributed setting is automatically performed on every call
+    to ``compute()``."""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.add_state("errors", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, pred_labels: torch.Tensor, target_labels: torch.Tensor) -> None:
+        pred_str = LabelData.from_labels(pred_labels).label_str
+        target_str = LabelData.from_labels(target_labels).label_str
+
+        self.errors += Levenshtein.distance(pred_str, target_str)
+        self.total += len(target_str)
+
+    def compute(self):
+        return self.errors.float() / self.total * 100.0
---
+++ emg2qwerty/utils.py
@@ -0,0 +1,35 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import Iterator
+
+from hydra.utils import instantiate
+from omegaconf import DictConfig, OmegaConf
+from torch import nn
+
+
+def instantiate_optimizer_and_scheduler(
+    params: Iterator[nn.Parameter],
+    optimizer_config: DictConfig,
+    lr_scheduler_config: DictConfig,
+):
+    optimizer = instantiate(optimizer_config, params)
+    scheduler = instantiate(lr_scheduler_config.scheduler, optimizer)
+    lr_scheduler = instantiate(lr_scheduler_config, scheduler=scheduler)
+    return {
+        "optimizer": optimizer,
+        "lr_scheduler": OmegaConf.to_container(lr_scheduler),
+    }
+
+
+def cpus_per_task(gpus_per_node: int, tasks_per_node: int, num_workers: int) -> int:
+    """Number of CPUs to request per task per node taking into account
+    the number of gpus and dataloading workers."""
+    gpus_per_task = gpus_per_node // tasks_per_node
+    if gpus_per_task <= 0:
+        return num_workers + 1
+    else:
+        return (num_workers + 1) * gpus_per_task
