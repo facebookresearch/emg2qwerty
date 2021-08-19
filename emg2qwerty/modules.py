bfd21c8053997d1246596c2c248104ba59d7a536
Viswanath Sivakumar <viswanath@fb.com>
2021-08-18 18:00:08 -0700

    Initial commit

---
+++ emg2qwerty/modules.py
@@ -0,0 +1,270 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import Sequence
+
+import torch
+from torch import nn
+
+
+class SpectrogramNorm(nn.Module):
+    """A `torch.nn.Module` that applies 2D batch normalization over spectrogram
+    per electrode channel per band. Inputs must be of shape
+    (T, N, num_bands, electrode_channels, frequency_bins).
+
+    With left and right bands and 16 electrode channels per band, spectrograms
+    corresponding to each of the 2 * 16 = 32 channels are normalized
+    independently using `nn.BatchNorm2d` such that stats are computed
+    over (N, freq, time) slices.
+
+    Args:
+        channels (int): Total number of electrode channels across bands
+            such that the normalization statistics are calculated per channel..
+            Should be equal to num_bands * electrode_chanels.
+    """
+
+    def __init__(self, channels: int) -> None:
+        super().__init__()
+        self.batch_norm = nn.BatchNorm2d(channels)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        T, N, bands, C, freq = inputs.shape  # (T, N, bands=2, C=16, freq)
+        x = inputs.movedim(0, -1)  # (N, bands=2, C=16, freq, T)
+
+        x = x.reshape(N, bands * C, freq, T)
+        x = self.batch_norm(x)
+        x = x.reshape(N, bands, C, freq, T)
+
+        return x.movedim(-1, 0)  # (T, N, bands=2, C=16, freq)
+
+
+class RotationInvariantMLP(nn.Module):
+    """A `torch.nn.Module` that takes an input tensor of shape
+    (T, N, electrode_channels, ...) corresponding to a single band, applies
+    an MLP after shifting/rotating the electrodes for each positional offset
+    in ``offsets``, and pools over all the outputs.
+
+    Returns a tensor of shape (T, N, mlp_features[-1]).
+
+    Args:
+        in_features (int): Number of input features to the MLP. For an input of
+            shape (T, N, C, ...), this should be equal to C * ... (that is,
+            the flattened size from the channel dim onwards).
+        mlp_features (list): List of integers denoting the number of
+            out_features per layer in the MLP.
+        pooling (str): Whether to apply mean or max pooling over the outputs
+            of the MLP corresponding to each offset. (default: "mean")
+        offsets (list): List of positional offsets to shift/rotate the
+            electrode channels by. (default: ``(-1, 0, 1)``).
+    """
+
+    def __init__(
+        self,
+        in_features: int,
+        mlp_features: Sequence[int],
+        pooling: str = "mean",
+        offsets: Sequence[int] = (-1, 0, 1),
+    ) -> None:
+        super().__init__()
+
+        assert len(mlp_features) > 0
+        mlp_layers = []
+        for out_features in mlp_features:
+            mlp_layers.extend(
+                [
+                    nn.Linear(in_features, out_features),
+                    nn.ReLU(inplace=True),
+                ]
+            )
+            in_features = out_features
+        self.mlp = nn.Sequential(*mlp_layers)
+
+        assert pooling in ["max", "mean"], f"Unsupported pooling: {pooling}"
+        self.pooling = pooling
+
+        self.offsets = offsets if len(offsets) > 0 else (0,)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        x = inputs  # (T, N, C, ...)
+
+        # Create a new dim for band rotation augmentation with each entry
+        # corresponding to the original tensor with its electrode channels
+        # shifted by one of ``offsets``:
+        # (T, N, C, ...) -> (T, N, rotation, C, ...)
+        x = torch.stack([x.roll(offset, dims=2) for offset in self.offsets], dim=2)
+
+        # Flatten features and pass through MLP:
+        # (T, N, rotation, C, ...) -> (T, N, rotation, mlp_features[-1])
+        x = self.mlp(x.flatten(start_dim=3))
+
+        # Pool over rotations:
+        # (T, N, rotation, mlp_features[-1]) -> (T, N, mlp_features[-1])
+        return x.max(dim=2).values if self.pooling == "max" else x.mean(dim=2)
+
+
+class MultiBandRotationInvariantMLP(nn.Module):
+    """A `torch.nn.Module` that applies a separate instance of
+    `RotationInvariantMLP` per band for inputs of shape
+    (T, N, num_bands, electrode_channels, ...).
+
+    Returns a tensor of shape (T, N, num_bands, mlp_features[-1]).
+
+    Args:
+        in_features (int): Number of input features to the MLP. For an input
+            of shape (T, N, num_bands, C, ...), this should be equal to
+            C * ... (that is, the flattened size from the channel dim onwards).
+        mlp_features (list): List of integers denoting the number of
+            out_features per layer in the MLP.
+        pooling (str): Whether to apply mean or max pooling over the outputs
+            of the MLP corresponding to each offset. (default: "mean")
+        offsets (list): List of positional offsets to shift/rotate the
+            electrode channels by. (default: ``(-1, 0, 1)``).
+        num_bands (int): ``num_bands`` for an input of shape
+            (T, N, num_bands, C, ...). (default: 2)
+        stack_dim (int): The dimension along which the left and right data
+            are stacked. (default: 2)
+    """
+
+    def __init__(
+        self,
+        in_features: int,
+        mlp_features: Sequence[int],
+        pooling: str = "mean",
+        offsets: Sequence[int] = (-1, 0, 1),
+        num_bands: int = 2,
+        stack_dim: int = 2,
+    ) -> None:
+        super().__init__()
+        self.num_bands = num_bands
+        self.stack_dim = stack_dim
+
+        # Separate MLP for each band
+        self.mlps = nn.ModuleList()
+        for i in range(num_bands):
+            self.mlps.append(
+                RotationInvariantMLP(
+                    in_features, mlp_features, pooling=pooling, offsets=offsets
+                )
+            )
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        assert inputs.shape[self.stack_dim] == self.num_bands
+
+        inputs_per_band = inputs.unbind(self.stack_dim)
+        outputs_per_band = [
+            mlp(input) for mlp, input in zip(self.mlps, inputs_per_band)
+        ]
+        return torch.stack(outputs_per_band, dim=self.stack_dim)
+
+
+class TDSConv2dBlock(nn.Module):
+    """A 2D temporal convolution block as per "Sequence-to-Sequence Speech
+    Recognition with Time-Depth Separable Convolutions, Hannun et al"
+    (https://arxiv.org/abs/1904.02619).
+
+    Args:
+        channels (int): Number of input and output channels. For an input of
+            shape (T, N, num_features), the invariant we want is
+            channels * width = num_features.
+        width (int): Input width. For an input of shape (T, N, num_features),
+            the invariant we want is channels * width = num_features.
+        kernel_width (int): The kernel size of the temporal convolution.
+    """
+
+    def __init__(self, channels: int, width: int, kernel_width: int) -> None:
+        super().__init__()
+
+        self.conv2d = nn.Conv2d(
+            in_channels=channels, out_channels=channels, kernel_size=(1, kernel_width)
+        )
+        self.relu = nn.ReLU()
+        self.layer_norm = nn.LayerNorm(channels * width)
+
+        self.channels = channels
+        self.width = width
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        T_in, N, C = inputs.shape  # TNC
+
+        # TNC -> NCT -> NcwT
+        x = inputs.movedim(0, -1).reshape(N, self.channels, self.width, T_in)
+        x = self.conv2d(x)
+        x = self.relu(x)
+        x = x.reshape(N, C, -1).movedim(-1, 0)  # NcwT -> NCT -> TNC
+
+        # Skip connection after downsampling
+        T_out = x.shape[0]
+        x += inputs[-T_out:]
+
+        # Layer norm over C
+        return self.layer_norm(x)  # TNC
+
+
+class TDSFullyConnectedBlock(nn.Module):
+    """A fully connected block as per "Sequence-to-Sequence Speech
+    Recognition with Time-Depth Separable Convolutions, Hannun et al"
+    (https://arxiv.org/abs/1904.02619).
+
+    Args:
+        num_features (int): ``num_features`` for an input of shape
+            (T, N, num_features).
+    """
+
+    def __init__(self, num_features: int) -> None:
+        super().__init__()
+
+        self.fc_block = nn.Sequential(
+            nn.Linear(num_features, num_features),
+            nn.ReLU(inplace=True),
+            nn.Linear(num_features, num_features),
+        )
+        self.layer_norm = nn.LayerNorm(num_features)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        x = inputs  # TNC
+        x = self.fc_block(x)
+        x += inputs
+        return self.layer_norm(x)  # TNC
+
+
+class TDSConvEncoder(nn.Module):
+    """A time depth-separable convolutional encoder composing a sequence
+    of `TDSConv2dBlock` and `TDSFullyConnectedBlock` as per
+    "Sequence-to-Sequence Speech Recognition with Time-Depth Separable
+    Convolutions, Hannun et al" (https://arxiv.org/abs/1904.02619).
+
+    Args:
+        num_features (int): ``num_features`` for an input of shape
+            (T, N, num_features).
+        block_channels (list): A list of integers indicating the number
+            of channels per `TDSConv2dBlock`.
+        kernel_width (int): The kernel size of the temporal convolutions.
+    """
+
+    def __init__(
+        self,
+        num_features: int,
+        block_channels: Sequence[int] = (24, 24, 24, 24),
+        kernel_width: int = 32,
+    ) -> None:
+        super().__init__()
+
+        assert len(block_channels) > 0
+        tds_conv_blocks = []
+        for channels in block_channels:
+            assert (
+                num_features % channels == 0
+            ), "block_channels must evenly divide num_features"
+            tds_conv_blocks.extend(
+                [
+                    TDSConv2dBlock(channels, num_features // channels, kernel_width),
+                    TDSFullyConnectedBlock(num_features),
+                ]
+            )
+        self.tds_conv_blocks = nn.Sequential(*tds_conv_blocks)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        return self.tds_conv_blocks(inputs)  # (T, N, num_features)
---
+++ emg2qwerty/utils.py
@@ -0,0 +1,43 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from pathlib import Path
+from typing import Iterator, Optional
+
+from hydra.utils import instantiate
+from omegaconf import DictConfig, OmegaConf
+from torch import nn
+
+
+def instantiate_optimizer_and_scheduler(
+    params: Iterator[nn.Parameter],
+    optimizer_config: DictConfig,
+    lr_scheduler_config: DictConfig,
+):
+    optimizer = instantiate(optimizer_config, params)
+    scheduler = instantiate(lr_scheduler_config.scheduler, optimizer)
+    lr_scheduler = instantiate(lr_scheduler_config, scheduler=scheduler)
+    return {
+        "optimizer": optimizer,
+        "lr_scheduler": OmegaConf.to_container(lr_scheduler),
+    }
+
+
+def get_last_checkpoint(checkpoint_dir: Path) -> Optional[Path]:
+    checkpoints = list(checkpoint_dir.glob("*.ckpt"))
+    if not checkpoints:
+        return None
+    return sorted(checkpoints, key=lambda p: p.stat().st_mtime)[-1]
+
+
+def cpus_per_task(gpus_per_node: int, tasks_per_node: int, num_workers: int) -> int:
+    """Number of CPUs to request per task per node taking into account
+    the number of gpus and dataloading workers."""
+    gpus_per_task = gpus_per_node // tasks_per_node
+    if gpus_per_task <= 0:
+        return num_workers + 1
+    else:
+        return (num_workers + 1) * gpus_per_task
