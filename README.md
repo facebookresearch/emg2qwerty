9d7d04c03eef2fe3124f2f9e8a6485c6d52d2c24
Viswanath Sivakumar <viswanath@fb.com>
2021-08-23 17:49:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# emg2qwerty
+
+Code accompanying `emg2qwerty` dataset along with baselines.
+
+### Setup
+
+```shell
+# Make sure you have [git-lfs installed](https://git-lfs.github.com/) (for pre-trained models)
+git lfs install
+
+git clone git@github.com:fairinternal/emg2qwerty.git && cd emg2qwerty/
+conda env create -f environment.yml
+
+ln -s /private/home/viswanath/oss/emg2qwerty-data-2021-08 data
+```
+
+### Training on FAIR Cluster
+
+Generic user model:
+```shell
+python -m emg2qwerty.train \
+  user=generic \
+  trainer.gpus=8 \
+  +cluster=slurm -m
+```
+
+Personalized user models:
+```shell
+python -m emg2qwerty.train \
+  user="glob(user*)" \
+  trainer.gpus=1 \
+  checkpoint="${HOME}/emg2qwerty/models/generic.ckpt" \
+  +cluster=slurm -m
+
+```
+
+### Testing
+
+Greedy decoding:
+```shell
+python -m emg2qwerty.train \
+  user=user0 \
+  decoder=ctc_greedy \
+  train=False \
+  trainer.gpus=0 \
+  checkpoint="${HOME}/emg2qwerty/models/user0.ckpt" \
+  hydra.launcher.mem_gb=64 \
+  +cluster=slurm -m
+```
+
+Beam-search decoding with 6-gram language model:
+```shell
+python -m emg2qwerty.train \
+  user=user0 \
+  decoder=ctc_beam \
+  train=False \
+  trainer.gpus=0 \
+  checkpoint="${HOME}/emg2qwerty/models/user0.ckpt" \
+  hydra.launcher.mem_gb=64 \
+  +cluster=slurm -m
+```
+
+The above uses a 6-gram character-level language model trained using kenlm on WikiText-103 raw corpus. To re-build the language model,
+1. Build kenlm from source: https://github.com/kpu/kenlm#compiling
+2. Download nltk punkt tokenizer: `python -c "import nltk; nltk.download('punkt')"`
+3. Run `./scripts/lm/build_char_lm.sh <ngram_order>`
---
+++ scripts/lm/build_char_lm.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Script to build a character-level n-gram language model using kenlm
+# from WikiText-103 raw corpus.
+#
+# Vocabulary is limited to lower-case alphabets only, and the
+# generated n-grams do not span word boundaries.
+#
+# Dependencies: Download kenlm (https://github.com/kpu/kenlm) to ~/kenlm
+# and build from source.
+#
+# Usage: ./build_char_lm.sh <NGRAM_ORDER>
+
+set -e
+
+if [ $# -lt 1 ]; then
+  echo "Usage: ./build_char_lm.sh <NGRAM_ORDER>"
+  exit 1
+fi
+
+NGRAM_ORDER=$1
+
+SRC_DIR="$(dirname $0)"
+ROOT_DIR="${SRC_DIR}/../.."
+OUT_DIR="${ROOT_DIR}/models"
+
+PREPROCESSOR="${SRC_DIR}/preprocess_char_lm.py"
+PREPROCESSED_DATA="wikitext-103-raw-preprocessed.txt"
+
+LM_ARPA="${OUT_DIR}/wikitext-103-${NGRAM_ORDER}-gram-char-lm.arpa"
+LM_BIN="${OUT_DIR}/wikitext-103-${NGRAM_ORDER}-gram-char-lm.bin"
+
+export PYTHONPATH="${PYTHONPATH}:${ROOT_DIR}"
+export PATH="${PATH}:~/kenlm/build/bin"
+
+# Download and preprocess wikitext-103 raw character level dataset:
+# https://blog.einstein.ai/the-wikitext-long-term-dependency-language-modeling-dataset/
+if [ ! -d wikitext-103-raw ]; then
+  echo "Downloading wikitext-103 raw character level dataset"
+  wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-raw-v1.zip
+  unzip wikitext-103-raw-v1.zip
+
+  echo "Preprocessing to ${PREPROCESSED_DATA}"
+  cat wikitext-103-raw/* | python ${PREPROCESSOR} > ${PREPROCESSED_DATA}
+fi
+
+mkdir -p ${OUT_DIR}
+
+echo -e "\nBuilding ${NGRAM_ORDER}-gram char-LM"
+lmplz -o ${NGRAM_ORDER} --discount_fallback < ${PREPROCESSED_DATA} > ${LM_ARPA}
+build_binary ${LM_ARPA} ${LM_BIN}
