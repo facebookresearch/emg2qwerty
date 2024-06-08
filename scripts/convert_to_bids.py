037ab54d4499bb80aa15374f5a8fcd0c27a57122
Viswanath Sivakumar <viswanath@meta.com>
2024-06-07 18:28:47 -0700

    Sync with fbsource mirror

--- scripts/convert_to_bids.py
+++ scripts/convert_to_bids.py
@@ -9,6 +9,7 @@ Script to convert the dataset from HDF5 to BIDS format (pretending it's EEG).
 https://bids-specification.readthedocs.io/en/stable/modality-specific-files/electroencephalography.html.
 """
 
+from concurrent.futures import as_completed, ProcessPoolExecutor
 from pathlib import Path
 
 import click
@@ -17,18 +18,17 @@ import mne_bids
 import numpy as np
 import pandas as pd
 import tqdm
-import yaml
+from emg2qwerty.charset import charset
 
 from emg2qwerty.data import EMGSessionData
 
 mne.set_log_level("WARNING")
 
 
-def get_raw(session_name: str, dataset_root: str) -> mne.io.Raw:
+def get_mne_raw(session_path: Path) -> mne.io.Raw:
     """Read data in HDF5 and get an MNE Raw object with keystrokes and prompts
     saved as mne.Annotations."""
-    fname = Path(dataset_root) / (session_name + ".hdf5")
-    session = EMGSessionData(fname)
+    session = EMGSessionData(session_path)
     label_data = session.ground_truth()
 
     ch_names = [f"emg{i}" for i in range(16)]
@@ -38,45 +38,67 @@ def get_raw(session_name: str, dataset_root: str) -> mne.io.Raw:
     info = mne.create_info(ch_names=ch_names, sfreq=sfreq, ch_types="eeg")
     raw = mne.io.RawArray(data.T, info)
 
-    # Deal with annot for keystrokes
+    # Deal with annotations for keystrokes
     idx = np.searchsorted(session["time"], label_data.timestamps)
-    # Fix idx as certain keys arrive after then final timestamp
+    # Fix idx as certain keys arrive after the final timestamp
     idx[idx >= len(session["time"])] = len(session["time"]) - 1
 
-    chars = list(label_data.text)
-    mapping = {
-        "⌫": "Key.backspace",
-        "⏎": "Key.enter",
-        "⇧": "Key.shift",
-        " ": "Key.space",
-    }
-    chars_ = [mapping.get(c, c) for c in chars]
-    # prefix with "key" to distinguish with prompts
-    chars_ = ["key/" + c for c in chars_]
-    annot = mne.Annotations(
-        onset=raw.times[idx], duration=np.zeros(len(idx)), description=chars_
+    keys = charset().str_to_keys(label_data.text)
+    # Prefix with "key" to distinguish from prompts
+    keys = [f"key/{key}" for key in keys]
+    annotation_keys = mne.Annotations(
+        onset=raw.times[idx],
+        duration=np.zeros(len(idx)),
+        description=keys,
     )
 
-    # Deal with annot for prompts
+    # Deal with annotations for prompts
     prompts = pd.DataFrame(session.prompts)
     prompts = prompts.query("name == 'text_prompt'")
     idx_start = np.searchsorted(session["time"], prompts.start)
     idx_end = np.searchsorted(session["time"], prompts.end)
-    # Fix idx as certain keys arrive after then final timestamp
+    # Fix idx as certain keys arrive after the final timestamp
     idx_start[idx_start >= len(session["time"])] = len(session["time"]) - 1
     idx_end[idx_end >= len(session["time"])] = len(session["time"]) - 1
     onset = raw.times[idx_start]
     duration = raw.times[idx_end] - raw.times[idx_start]
     description = prompts.payload.apply(pd.Series).text.str.replace("⏎", "\\n").values
-    # prefix with "prompt" to distinguish with keys
+    # Prefix with "prompt" to distinguish from keys
     description = ["prompt/" + d for d in description]
-    annot_prompts = mne.Annotations(
-        onset=onset, duration=duration, description=description
+    annotation_prompts = mne.Annotations(
+        onset=onset,
+        duration=duration,
+        description=description,
     )
 
-    annot = annot + annot_prompts
-    raw.set_annotations(annot)
+    annotations = annotation_keys + annotation_prompts
+    raw.set_annotations(annotations)
     return raw
+
+
+def convert_to_bids(
+    subject_idx: int,
+    session_idx: int,
+    session_path: Path,
+    bids_root: str,
+) -> None:
+    """Convert the HDF5 emg2qwerty session file located at `session_path` to BIDS format
+    and output to `bids_root` directory.
+    """
+    raw = get_mne_raw(session_path)
+    bids_path = mne_bids.BIDSPath(
+        subject=f"{subject_idx + 1:02d}",
+        session=f"{session_idx + 1:02d}",
+        task="emg2qwerty",
+        root=bids_root,
+    )
+    mne_bids.write_raw_bids(
+        raw=raw,
+        bids_path=bids_path,
+        overwrite=True,
+        format="BrainVision",
+        allow_preload=True,
+    )
 
 
 @click.command()
@@ -92,25 +114,34 @@ def get_raw(session_name: str, dataset_root: str) -> mne.io.Raw:
     default=Path(__file__).parents[1].joinpath("bids_data"),
     help="BIDS dataset root directory (defaults to 'bids_data' folder)",
 )
-def main(
-    dataset_root: str,
-    bids_root: str,
-):
-    df = pd.read_csv(Path(dataset_root).joinpath("metadata.csv"))
-    df.quality_check_tags = df.quality_check_tags.apply(yaml.safe_load)
+@click.option(
+    "--max-workers",
+    type=int,
+    default=1,
+    help="Number of workers for parallelism",
+)
+def main(dataset_root: str, bids_root: str, max_workers: int):
+    executor = ProcessPoolExecutor(max_workers=max_workers)
+    futures = []
 
-    for ix_subject, (_, df_user) in enumerate(df.groupby("user")):
-        for ix_session in tqdm.tqdm(range(len(df_user.session.values))):
-            raw = get_raw(df_user.iloc[ix_session].session, dataset_root)
-            bids_path = mne_bids.BIDSPath(
-                subject=f"{ix_subject + 1:02d}",
-                session=f"{ix_session + 1:02d}",
-                task="typing",
-                root=bids_root,
+    df = pd.read_csv(Path(dataset_root).joinpath("metadata.csv"))
+    users = sorted(df["user"].unique())
+    for subject_idx, user in enumerate(users):
+        sessions = sorted(df[df["user"] == user].session)
+        for session_idx, session in enumerate(sessions):
+            session_path = Path(dataset_root).joinpath(f"{session}.hdf5")
+            futures.append(
+                executor.submit(
+                    convert_to_bids,
+                    subject_idx=subject_idx,
+                    session_idx=session_idx,
+                    session_path=session_path,
+                    bids_root=bids_root,
+                )
             )
-            mne_bids.write_raw_bids(
-                raw, bids_path, overwrite=True, format="BrainVision", allow_preload=True
-            )
+
+    for _ in tqdm.tqdm(as_completed(futures), total=len(futures)):
+        pass
 
 
 if __name__ == "__main__":
