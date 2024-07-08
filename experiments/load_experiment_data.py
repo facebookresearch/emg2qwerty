0ce2a8e09b3e5c72c795ddaf069723b4872e1088
Nick Mehlman <nmehlman@usc.edu>
2024-07-08 14:51:28 +0000

    Additional experiment updates

--- experiments/load_experiment_data.py
+++ experiments/load_experiment_data.py
@@ -5,6 +5,7 @@ import pickle
 import subprocess
 import re
 import json
+import yaml
 
 def parse_submitit_output(submitit_output_path: str) -> dict:
 
@@ -43,6 +44,20 @@ def parse_and_save_metrics(submitit_output_path: str, save_path: str = './metric
     # Save as JSON
     json.dump(metrics, open(save_path, 'w'), indent=2)
 
+def get_config_from_log(submitit_output_path: str) -> dict:
+
+    output = subprocess.check_output(['head', '-n', '250', submitit_output_path]) # Read last 50 lines
+    output_lines = output.decode('utf-8').split('\n')
+
+    config_start_line = [i for i, line in enumerate(output_lines) if line.startswith('Config')][0]
+    config_end_line = [i for i, line in enumerate(output_lines) if 'verbose' in line][0] + 1
+
+    output_str = "\n".join(output_lines[config_start_line:config_end_line])
+
+    config = yaml.safe_load(output_str)
+
+    return config
+
 def parse_and_save_personalized_model_metrics(submitit_root_path: str, save_root: str = '.'):
 
     for i, sub_dir in enumerate(sorted(os.listdir(submitit_root_path))):
@@ -65,13 +80,3 @@ def parse_and_save_personalized_model_metrics(submitit_root_path: str, save_root
             save_path=os.path.join(save_root, f"user{user_id}_metrics.json")
         )
         
-
-
-if __name__ == "__main__":
-
-    import sys
-
-    parse_and_save_personalized_model_metrics(
-        "/private/home/nmehlman/emg2qwerty/logs/2024-07-04/02/submitit_logs",
-        "/private/home/nmehlman/emg2qwerty/experiments/2024070402/metrics"
-    )
